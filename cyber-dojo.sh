4fb9235b4ef851516f15600158d46c1bafba52e9
Jon Jagger <jon@jaggersoft.com>
2018-04-11 07:27:02 +0100

    refactoring

--- cyber-dojo.sh
+++ cyber-dojo.sh
@@ -1,3 +1,6 @@
+set -e
 # -E == preprocess only
 # -P == dont show #line's
 gcc -std=c99 -E -P fizz_buzz.tests.h
+
+echo All tests passed
--- fizz_buzz.tests.h
+++ fizz_buzz.tests.h
@@ -1,7 +1,7 @@
 #include "fizz_buzz.h"
 
 #define ASSERT(e,a)   ASSERT_(e,a)
-#define ASSERT_(e,a)  STR(e ## _ ## a)
+#define ASSERT_(e,a)  STR(ASSERT_ ## e ## _ ## a)
 
 #include ASSERT(FIZZ_BUZZ_1_EQUALS, FIZZ_BUZZ(1))
 #include ASSERT(FIZZ_BUZZ_3_EQUALS, FIZZ_BUZZ(3))
